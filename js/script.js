8934f37840ccaa22c02a74af47795f3d0b53085e
Eldon Costa <eldoncosta1@gmail.com>
2021-01-20 18:05:55 -0300

    Corrigindo bug da cobrinha passeando fora do canvas e adicionando comentários

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+// cobrinha criada no centro do canvas com tamanho definido no index.html
 snake[0] = {
   x: 8 * box,
   y: 8 * box
@@ -43,11 +44,13 @@ function update(event) {
 }
 
 function iniciarJogo() {  
-  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+  // Ao chegar no fim do canvas, aparece do outro lado (corrigindo bug que deixava a cobra passeando fora do canva)
+  if (snake[0].x > 15 * box && direction != "left") snake[0].x = 0;
+  if (snake[0].x < 0 && direction != "right") snake[0].x = 15 * box;
+  if (snake[0].y > 15 * box && direction != "up") snake[0].y = 0;
+  if (snake[0].y < 0 && direction != "down") snake[0].y = 15 * box;
 
+  // se a posição 0 (cabeça) se chocar com o corpo, ela para o jogo
   for (let i = 1; i < snake.length; i++) {
     if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
       clearInterval(jogo);
@@ -59,17 +62,25 @@ function iniciarJogo() {
   criarCobrinha();
   drawFood();
 
+  // posição da cabeça da cobrinha
   let snakeX = snake[0].x;
   let snakeY = snake[0].y
 
+  // movimento da cobrinha
   if (direction == "right") snakeX += box;
   if (direction == "left") snakeX -= box;
   if (direction == "up") snakeY -= box;
   if (direction == "down") snakeY += box;
 
+  // crescimento da cobrinha
   if (snakeX != food.x || snakeY != food.y) {
+    /**
+     * caso a posição da cobrinha seja diferente da comida,
+     * ela continua em movimento (removendo um item do array)
+     */
     snake.pop();
   } else {
+    // caso contrário, não remove o item do array (aumenta de tamanho e a modia muda para outra posição aleatória)
     food.x = Math.floor(Math.random() * 15 + 1) * box;
     food.y = Math.floor(Math.random() * 15 + 1) * box;
   }
