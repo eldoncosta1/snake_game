cf0d8a624e9e479ad8510d381c8479b3578ae3bd
Eldon Costa <eldoncosta1@gmail.com>
2021-01-20 15:19:21 -0300

    Criando a cobrinha

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+}
 
 function criarBG() {
   context.fillStyle = "lightgreen";
@@ -10,4 +15,12 @@ function criarBG() {
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarCobrinha() {
+  for (let i = 0; i < snake.length; i++) {
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
 criarBG();
+criarCobrinha();
