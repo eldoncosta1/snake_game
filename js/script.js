c96d1cea9254aa848143606434675f9e2e1d736c
Eldon Costa <eldoncosta1@gmail.com>
2021-01-20 16:02:07 -0300

    Criação da comida

--- js/script.js
+++ js/script.js
@@ -7,6 +7,10 @@ snake[0] = {
   y: 8 * box
 }
 let direction = "right";
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
   context.fillStyle = "lightgreen";
@@ -21,6 +25,11 @@ function criarCobrinha() {
     context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
+}
+
+function drawFood() {
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -42,6 +51,7 @@ function iniciarJogo() {
 
   criarBG();
   criarCobrinha();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y
