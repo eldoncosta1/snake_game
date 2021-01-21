8675545adbbe01fb62c51e70f336c8b2d38a0335
Eldon Costa <eldoncosta1@gmail.com>
2021-01-21 09:50:04 -0300

    Corrigindo função para finalizar jogo quando a cobrinha encosta nela mesmo

--- js/script.js
+++ js/script.js
@@ -56,10 +56,12 @@ function drawFood() {
 document.addEventListener('keydown', update);
 
 function update(event) {
-  if (event.keyCode == 37 && direction != "right") direction = "left";
-  if (event.keyCode == 38 && direction != "down") direction = "up";
-  if (event.keyCode == 39 && direction != "left") direction = "right";
-  if (event.keyCode == 40 && direction != "up") direction = "down";
+  if (!fim) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+  }
 
 }
 
