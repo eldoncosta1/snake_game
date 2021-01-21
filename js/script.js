1691ccd245805960003689d4c2521f490fbadd27
Eldon Costa <eldoncosta1@gmail.com>
2021-01-21 09:59:48 -0300

    Jogo iniciando parado. É necessário movimentar uma das direções para iniciá-lo

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ snake[0] = {
   x: 8 * box,
   y: 8 * box
 }
-let direction = "right";
+let direction; // jogo inicia parado, só começa a movimentar quando o jogado pressiona uma seta de direção
 let food = {
   x: Math.floor(Math.random() * 15 + 1) * box,
   y: Math.floor(Math.random() * 15 + 1) * box
